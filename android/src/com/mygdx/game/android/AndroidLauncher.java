868c4ef7cfcce844bd3855f7d4c91b6ff950f906
Justin <strukjustin@gmail.com>
2015-12-02 10:45:12 -0500

    fixed a tiny bug.

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.Release1;
+import com.mygdx.game.ScreenHandler;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Release1(), config);
+		initialize(new ScreenHandler(), config);
 	}
 }
